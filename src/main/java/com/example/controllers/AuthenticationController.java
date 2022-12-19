6a752ee6a737c1930b42e10140e1d7e328bb7354
Tim Mackey <xenserverarmy@users.noreply.github.com>
2022-12-19 17:37:25 -0500

    Update AuthenticationController.java

--- src/main/java/com/example/controllers/AuthenticationController.java
+++ src/main/java/com/example/controllers/AuthenticationController.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AuthenticationController {
 
-	@RequestMapping("/login", method = RequestMethod.GET)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(final Model model) {
 		return "login";
 	}
 
-	@RequestMapping("/", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(final Model model) {
 		return "redirect:/dashboard";
 	}
 
-	@RequestMapping("/403", method = RequestMethod.GET)
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
 	public String error403(final Model model) {
 		return "error403";
 	}
