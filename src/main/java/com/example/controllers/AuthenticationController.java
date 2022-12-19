16012dde28bacdd48ae06fdc1b564eff4ecd9746
Tim Mackey <xenserverarmy@users.noreply.github.com>
2022-12-19 17:17:30 -0500

    Force to GET for requests

--- src/main/java/com/example/controllers/AuthenticationController.java
+++ src/main/java/com/example/controllers/AuthenticationController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AuthenticationController {
 
-	@RequestMapping("/login")
+	@RequestMapping("/login", method = RequestMethod.GET)
 	public String login(final Model model) {
 		return "login";
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/", method = RequestMethod.GET)
 	public String home(final Model model) {
 		return "redirect:/dashboard";
 	}
 
-	@RequestMapping("/403")
+	@RequestMapping("/403", method = RequestMethod.GET)
 	public String error403(final Model model) {
 		return "error403";
 	}
